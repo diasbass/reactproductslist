ab88d78951644ee8f75776abef478034fc77a4d9
Rodrigo Dias <diasbass@hotmail.com>
2019-08-22 18:05:35 -0300

    Ajuste class

--- reactproductlist/src/Header.js
+++ reactproductlist/src/Header.js
@@ -3,9 +3,9 @@ import React from "react";
 const Header = () => {
   return(
     <nav>
-    <div class="nav-wrapper indigo lighten-1">
-      <a href="/" class="brand-logo">Cadastro</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
+    <div className="nav-wrapper indigo lighten-1">
+      <a href="/" className="brand-logo">Cadastro</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li><a href="/">Autores</a></li>
         <li><a href="/">Cursos</a></li>
         <li><a href="/">Sobre</a></li>

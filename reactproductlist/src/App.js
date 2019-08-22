5ed27a4564d4eb99c34b382bdc9b3505cd0a4eeb
Rodrigo Dias <diasbass@hotmail.com>
2019-08-22 17:15:07 -0300

    Add Materialize

--- reactproductlist/src/App.js
+++ reactproductlist/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-//import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
 import ProductList from './ProductList';
 import Formulario from './Formulario';
 
